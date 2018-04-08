385d7e73d1d48e8f1aa965ecc5b6795f26d0bb22
Eresto <ernestojosecg@gmail.com>
2018-04-08 13:36:42 +0200

    Create_produers

--- db/migrate/20180402095438_create_producers.rb
+++ db/migrate/20180402095438_create_producers.rb
@@ -1,7 +1,7 @@
 class CreateProducers < ActiveRecord::Migration
   def change
     create_table :producers do |t|
-      t.string:name, :limit => 255, :null => false, :unique=> true
+      t.string :name, :limit => 255, :null => false, :unique=> true
       t.timestamps :null => false
     end
   end
