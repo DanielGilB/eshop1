21a7ac4a66090f754429d6b0d51dbfd086904967
Eresto <ernestojosecg@gmail.com>
2018-04-23 12:26:45 +0200

    ojala viole a fede2

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,6 @@
 class Artist < ActiveRecord::Base
   #validates_presence_of :first_name, :last_name
+  has_and_belongs_to_many :discs
   validates_presence_of :first_name
   validates_length_of :first_name, :in => 2..255
 
--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,4 +1,5 @@
 class Producer < ActiveRecord::Base
+	has_many :discs
 	validates_presence_of :name
 	validates_uniqueness_of :name
 	validates_length_of :name, :in => 2..255
