e8554b0e985a13d6b80be53669d4026b82206b83
Daniel Gil Bustillo <danielgilbustillo@gmail.com>
2018-04-30 13:26:44 +0200

    7ok

--- test/controllers/cart_controller_test.rb
+++ test/controllers/cart_controller_test.rb
@@ -1,19 +1,30 @@
 require 'test_helper'
 
 class CartControllerTest < ActionController::TestCase
-  test "should get add" do
-    get :add
-    assert_response :success
+  test "add" do
+    assert_difference(CartItem, :count) do
+      post :add, :id => 4
+    end
+    assert_response :redirect
+    assert_redirected_to :controller => 'catalog'
+    assert_equal 1, Cart.find(@request.session[:cart_id]).cart_items.size
   end
 
-  test "should get remove" do
-    get :remove
-    assert_response :success
+  test "remove" do
+    post :add, :id => 4
+    assert_equal [Disc.find(4)], Cart.find(@request.session[:cart_id]).discs
+
+    post :remove, :id => 4
+    assert_equal [], Cart.find(@request.session[:cart_id]).discs
   end
 
-  test "should get clear" do
-    get :clear
-    assert_response :success
-  end
+  test "clear" do
+    post :add, :id => 4
+    assert_equal [Disc.find(4)], Cart.find(@request.session[:cart_id]).discs
 
+    post :clear
+    assert_response :redirect
+    assert_redirected_to :controller => 'catalog'
+    assert_equal [], Cart.find(@request.session[:cart_id]).discs
+  end
 end
