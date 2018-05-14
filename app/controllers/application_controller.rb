6219919f6e3f887a956934f7d1fdabbd49eddc4e
Eresto <ernestojosecg@gmail.com>
2018-05-14 12:20:34 +0200

    User Contr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,45 @@ class ApplicationController < ActionController::Base
     end
   end
 
+def current_user_session
+    logger.debug "ApplicationController::current_user_session"
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
 
+  def current_user
+    logger.debug "ApplicationController::current_user"
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.user
+  end
+
+  def require_user
+    logger.debug "ApplicationController::require_user"
+    unless current_user
+      store_location
+      flash[:notice] = "You must be logged in to access this page."
+      redirect_to :controller => '/user_sessions', :action => 'new'
+      return false
+    end
+  end
+
+  def require_no_user
+    logger.debug "ApplicationController::require_no_user"
+    if current_user
+      store_location
+      flash[:notice] = "You must be logged out to access this page."
+      redirect_to :controller => 'about', :action => :index
+      return false
+    end
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
+  end
   
 end
--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UserSessionTest < ActiveSupport::TestCase
+class UserControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
