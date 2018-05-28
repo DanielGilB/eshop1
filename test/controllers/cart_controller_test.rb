baa3c68afe2614229b3e8f0e2d91b56763fda89f
Daniel Gil Bustillo <danielgilbustillo@gmail.com>
2018-05-28 12:06:21 +0200

    ajax test

--- test/controllers/cart_controller_test.rb
+++ test/controllers/cart_controller_test.rb
@@ -28,4 +28,29 @@ class CartControllerTest < ActionController::TestCase
     assert_redirected_to :controller => 'catalog'
     assert_equal [], Cart.find(@request.session[:cart_id]).discs
   end
+
+  test "add_xhr" do
+  	assert_difference(CartItem, :count) do 
+  		xhr :post, :add, :id => 4
+  	end
+  	assert_response :success
+  	assert_equal 1, Cart.find(@request.session[:cart_id]).cart_items.size			
+  end  
+  test "remove_xhr" do
+  	xhr :post, :add, :id => 4
+  	assert_equal [Disc.find(4)], Cart.find(@request.session[:cart_id]).discs
+  	
+  	xhr :post, :remove, :id => 4
+  	assert_equal [], Cart.find(@request.session[:cart_id]).discs			
+  end
+
+  test "clear_xhr" do
+ 	xhr :post, :add, :id => 4
+ 	xhr :post, :add, :id => 1
+  	assert_equal [Disc.find(4), Disc.find(1)], Cart.find(@request.session[:cart_id]).discs
+
+  	xhr :post, :clear
+  	assert_response :success
+    assert_equal [], Cart.find(@request.session[:cart_id]).discs		
+  end	
 end
